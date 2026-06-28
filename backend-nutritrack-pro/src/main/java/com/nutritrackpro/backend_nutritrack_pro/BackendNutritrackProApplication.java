536f0cf55dcedde7699cbd34d8d26caf5c90db75
Javier Chaler Romero <javichr11@gmail.com>
2026-06-28 17:56:51 +0200

    init repo

---
+++ backend-nutritrack-pro/src/main/java/com/nutritrackpro/backend_nutritrack_pro/BackendNutritrackProApplication.java
@@ -0,0 +1,13 @@
+package com.nutritrackpro.backend_nutritrack_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNutritrackProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNutritrackProApplication.class, args);
+	}
+
+}
---
+++ backend-nutritrack-pro/src/test/java/com/nutritrackpro/backend_nutritrack_pro/BackendNutritrackProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutritrackpro.backend_nutritrack_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNutritrackProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
